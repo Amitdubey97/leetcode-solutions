f92a70244bc00fe2726270e3afddbef52c9a598d
Amitdubey97 <133532387+Amitdubey97@users.noreply.github.com>
2026-08-21 15:19:34 +0530

    Time: 0 ms (100%), Space: 8.5 MB (54.55%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,22 +1,17 @@
+
 class Solution {
 public:
     int reverse(int x) {
-        int rev = 0;
-
-        while(x != 0){
-            int digit = x%10;
-            x= x/10;
-             // check for overflow
-             if(rev > INT_MAX / 10 ||(rev == INT_MAX / 10 && digit > 7)) // for positive no.
-             return 0;
-
-             if(rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))// for negative no.
-             return 0;
-
-             rev = rev * 10 + digit;
+        long long int digit;
+        long long int y=0;
+        while(x!=0){
+            digit=x%10;
+            x=x/10;
+            y=y*10+digit;
         }
-        return rev;
+        if (y > 2147483647 || y < -2147483648)  // for overflow condition
+            return 0;
 
+    return y;
     }
-
 };
