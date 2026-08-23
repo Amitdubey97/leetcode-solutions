c1c7f4964c06b902fad2104ad1a7ed069e6ca8a0
Amitdubey97 <133532387+Amitdubey97@users.noreply.github.com>
2026-08-23 21:17:53 +0530

    Time: 0 ms (100%), Space: 14 MB (67.83%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int ql =0,qr = 0; 
+        long long diff = 0;
+
+        for(int i =0;i<n/2;i++){ // first half
+            if(num[i]=='?') ++ql;
+            else diff += num[i] -'0';
+        }
+        for(int i=n/2; i<n; i++){ // sercond half
+            if(num[i]=='?') ++qr;
+            else diff -= num[i] -'0';
+
+        }
+        if((ql +qr)&1) // agar '?' ka count odd to alice hamesha jeetega
+        return true;
+
+        return diff != (9*(qr-ql)) >>1; 
+    }
+};
