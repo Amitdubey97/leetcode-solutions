15f2c07f7cd2c5a453a6f95648d196f4017d36e2
Amitdubey97 <133532387+Amitdubey97@users.noreply.github.com>
2026-08-25 12:20:49 +0530

    Update 3718-smallest-missing-multiple-of-k.cpp

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -3,13 +3,15 @@ public:
     int missingMultiple(vector<int>& nums, int k) {
          int n = nums.size();
          unordered_set<int> st(nums.begin(), nums.end());
-         int multiple = k;
+         int multiple = k; //k ke multiples check krna start karo
 
          while(true){
+
+            //agar multiple set nahi hai to ye smallest missing multiple hai
             if(st.find(multiple) == st.end()){
                 return multiple;
             }
-            multiple += k;
+            multiple += k; // next multiple
          }
     }
 };
