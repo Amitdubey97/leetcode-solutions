bb4884d6caec9d9f02d2d445f5dafa68054ed0be
Amitdubey97 <133532387+Amitdubey97@users.noreply.github.com>
2026-08-31 20:54:00 +0530

    Time: 0 ms (100%), Space: 8.4 MB (20.93%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -9,12 +9,22 @@ word ke sare charachters distint hai . total 8 keys(2-9)
 class Solution {
 public:
     int minimumPushes(string word) {
-       int n = word.size();
+      /* int n = word.size();
        int ans = 0;
 
        for(int i =0; i<n; i++){
         ans+= (i/8)+1; // 8 charcters hai isiliye 1 iteration me 1 push ... so on
        } 
+       return ans;*/
+
+       int n = word.size();
+
+       int q = n/8;
+       int r = n%8;
+
+       int ans = 8*q*(q+1)/2;
+       ans+= r*(q+1);
+
        return ans;
     }
 };
